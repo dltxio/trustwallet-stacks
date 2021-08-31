11f3b8160203aca7a27b72c992c2f90ec2d159c5
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-08-31 14:04:31 +1000

    update android unit tests for Stacks

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
@@ -96,5 +96,6 @@ class CoinAddressDerivationTests {
         OASIS -> assertEquals("oasis1qzcpavvmuw280dk0kd4lxjhtpf0u3ll27yf7sqps", address)
         THORCHAIN -> assertEquals("thor1c8jd7ad9pcw4k3wkuqlkz4auv95mldr2kyhc65", address)
         BLUZELLE -> assertEquals("bluzelle1xccvees6ev4wm2r49rc6ptulsdxa8x8jfpmund", address)
+        STACKS -> assertEquals("ST38SNFBDDBE80Z7VVTPMD5R48QAJ0R3QW2S84V4E", address)
     }
 }
